21a2d699d6b889b4200741c5e7bb85a60a8b08cc
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-05-05 16:24:21 +0200

    Add DisjointSets project

---
+++ DisjointSets/DisjointSets/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DisjointSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
