0fec376324771e87cc073b8a06f1c871ad783cb0
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-05-05 17:51:23 +0200

    Done first version

--- DisjointSets/DisjointSets/Program.cs
+++ DisjointSets/DisjointSets/Program.cs
@@ -6,7 +6,14 @@ namespace DisjointSets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int vertices = 6;
+            var graph = new Graph(vertices);
+            graph.AddEgde(0, 1);
+            graph.AddEgde(0, 2);
+            graph.AddEgde(1, 3);
+            graph.AddEgde(4, 5);
+            Console.WriteLine("Disjoint Sets: ");
+            graph.DisjointSets();
         }
     }
 }
