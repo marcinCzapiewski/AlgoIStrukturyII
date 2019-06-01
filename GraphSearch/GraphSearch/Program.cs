8566d6595305b9f165e6fc6eed9fc55971f8421f
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2019-06-01 19:28:20 +0200

    Create graph search project

---
+++ GraphSearch/GraphSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GraphSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
